7682d03822138d260eb8ea33d7b472dc39fda91e
Adolf Mayer <4dolf.mayer@gmail.com>
2026-02-10 22:13:19 +0100

    JS pamet

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 console.log('JS is working');
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM ready");
+
+  document
+    .querySelectorAll("input, select, textarea")
+    .forEach(el => {
+      const saved = localStorage.getItem(el.name);
+      if (saved !== null) {
+        if (el.type === "checkbox") {
+          el.checked = saved === "true";
+        } else {
+          el.value = saved;
+        }
+      }
+
+      el.addEventListener("change", () => {
+        if (el.type === "checkbox") {
+          localStorage.setItem(el.name, el.checked);
+        } else {
+          localStorage.setItem(el.name, el.value);
+        }
+      });
+    });
+});
